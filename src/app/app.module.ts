d6a42860d3f4f6d80df9e7a8b571199e677406c1
jaime <pepefloress.1999@gmail.com>
2023-12-14 16:04:56 -0600

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { routingModule } from './app.rouitng.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    routingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
