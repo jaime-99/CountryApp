a29576647dc1e79218a9bc6ab58c841098c94188
jaime <pepefloress.1999@gmail.com>
2024-01-28 16:40:51 -0600

    se agrega para poder buscar sin presionr enter

--- src/app/shared/components/searchBox/searchBox.component.ts
+++ src/app/shared/components/searchBox/searchBox.component.ts
@@ -1,12 +1,25 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-search-box',
   templateUrl: './searchBox.component.html',
   styleUrls: ['./searchBox.component.css'],
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit {
+  //tipo especial de observable
+  private debouncer :Subject<string> = new Subject<string>();
+  ngOnInit(): void {
+    this.debouncer
+    .pipe(
+      debounceTime(300)
+    )
+    .subscribe(value=>{
+      this.ondebounce.emit(value);
+    });
+
+  }
 
   @Input()public  placeholder:string = ''
 
@@ -14,14 +27,19 @@ export class SearchBoxComponent {
   public tagInput!: ElementRef<HTMLInputElement>;
 
   @Output() onValue: EventEmitter<string> = new EventEmitter<string>();
+  //este es para que escriba y emita el evento
+  @Output() ondebounce: EventEmitter<string> = new EventEmitter<string>();
 
 
   emitirEvento(value:string): void {
-    this.onValue.emit(value);
-    //ejemplo
-
-
+    this.onValue.emit(value); 
   }
+
+  onkeyPress(searchTerm:string){
+    this.debouncer.next(searchTerm);
+  }
+  
+
 
 
 
